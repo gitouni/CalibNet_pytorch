edc3efadd52fad6a670b0bbacbe2cc7b3ceed933
gitouni <30369762+gitouni@users.noreply.github.com>
2024-02-24 17:20:01 +0000

    Update dataset.py
    fixed a serve bug in the kitti frameloader

--- dataset.py
+++ dataset.py
@@ -108,10 +108,8 @@ class BaseKITTIDataset(Dataset):
             dict_len = json.load(f)
         frame_list = []
         for seq in seqs:
-            frame = list(range(0,dict_len[seq],skip_frame))
-            cut_index = len(frame)%batch_size
-            if cut_index > 0:
-                frame = frame[:-cut_index]
+            num_frames = batch_size * (dict_len[seq] // batch_size)
+			frame = list(range(0, num_frames, skip_frame))
             frame_list.append(frame)
         self.kitti_datalist = [pykitti.odometry(basedir,seq,frames=frame) for seq,frame in zip(seqs,frame_list)]  
         # concat images from different seq into one batch will cause error
